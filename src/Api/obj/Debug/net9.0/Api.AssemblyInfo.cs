5f25cacc7297d4ec7d43139356ae6a824c58e71b
xavieraxo <lucerojavier@live.com>
2026-02-07 21:51:36 -0300

    Implement admin password reset functionality for production environment, ensuring secure password management for users.

--- src/Api/obj/Debug/net9.0/Api.AssemblyInfo.cs
+++ src/Api/obj/Debug/net9.0/Api.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3acd0daa40579f202c5df9c0cdd500546f6440d1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2cfcedfffc289b55ad40fda75b93c3301ea93186")]
 [assembly: System.Reflection.AssemblyProductAttribute("Api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
